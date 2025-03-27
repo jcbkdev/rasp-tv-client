674920c063a87e66d813abfc9695aa2d3f8f2623
jcbkdev <kubkon134@gmail.com>
2025-03-27 16:33:05 +0100

    player prototype

---
+++ src/ui/components/VideoPlayer.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useRef } from "react";
+import videojs from "video.js";
+import Player from "video.js/dist/types/player";
+import "video.js/dist/video-js.css";
+
+async function followRedirect(url: string): Promise<string> {
+    try {
+        // Use fetch to get the URL, allowing automatic redirect following
+        const response = await fetch(url, {
+            method: "GET",
+            redirect: "follow", // This follows redirects automatically
+        });
+
+        // Return the final URL after following redirects
+        return response.url;
+    } catch (error) {
+        console.error("Error following redirect:", error);
+        throw error; // Propagate error if something goes wrong
+    }
+}
+
+export default function VideoPlayer() {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const playerRef = useRef<Player>(null);
+
+    useEffect(() => {
+        if (!playerRef.current) {
+            const videoElement = document.createElement("video-js");
+            videoElement.classList.add("vjs-matrix");
+            videoElement.classList.add("rasptv-player");
+
+            containerRef.current?.appendChild(videoElement);
+
+            const player = (playerRef.current = videojs(
+                videoElement,
+                {
+                    controls: false,
+                    autoplay: true,
+                },
+                () => {
+                    videojs.log("player is ready");
+                    followRedirect("url").then((url) => {
+                        player.autoplay(true);
+                        player.src(url);
+                    });
+                }
+            ));
+        } else {
+            const player = playerRef.current;
+
+            followRedirect("url").then((url) => {
+                player.autoplay(true);
+                player.src(url);
+            });
+        }
+    }, [containerRef]);
+
+    useEffect(() => {
+        if (playerRef.current) {
+            playerRef.current.ready(() => {
+                console.log("ready");
+                playerRef.current?.play();
+            });
+        }
+    }, [playerRef]);
+
+    return (
+        <div data-vjs-player>
+            <div ref={containerRef}></div>
+        </div>
+    );
+}
---
+++ src/ui/pages/player/Player.tsx
@@ -0,0 +1,9 @@
+import VideoPlayer from "../../components/VideoPlayer";
+
+export default function Player() {
+    return (
+        <div className="player-container">
+            <VideoPlayer />
+        </div>
+    );
+}
